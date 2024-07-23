c294829a87213bcb45f91633ac47bab9698bbc7d
HR Farmer <58487401+hrfarmer@users.noreply.github.com>
2024-07-23 16:59:39 -0500

    make popup dismiss on click outside of popup

--- app/frontend/src/components/PopupContainer.tsx
+++ app/frontend/src/components/PopupContainer.tsx
@@ -1,10 +1,11 @@
 import "assets/css/popup.css";
 import { PopupContext, PopupContextType } from "contexts/PopupContext";
 import { AnimatePresence, motion } from "framer-motion";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 
 export default function PopupContainer() {
   const { popup, setPopup } = useContext(PopupContext) as PopupContextType;
+  const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     function handleEsc(event: KeyboardEvent) {
@@ -12,10 +13,19 @@ export default function PopupContainer() {
         setPopup(null);
       }
     }
+
+    function handleOutsideClick(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setPopup(null);
+      }
+    }
+
     window.addEventListener("keydown", handleEsc);
+    document.addEventListener("mousedown", handleOutsideClick);
 
     return () => {
       window.removeEventListener("keydown", handleEsc);
+      document.removeEventListener("mousedown", handleOutsideClick);
     };
   }, []);
   return (
@@ -33,7 +43,7 @@ export default function PopupContainer() {
             animate={{ opacity: 1, y: 0, scale: 1 }}
             exit={{ opacity: 0, y: 10, scale: 0.4 }}
           >
-            <div className="popup-content">
+            <div className="popup-content" ref={ref}>
               <div className="popup-heading">
                 <h1>{popup.title}</h1>
                 <div onClick={() => setPopup(null)}>X</div>
