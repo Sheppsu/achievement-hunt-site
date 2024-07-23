8adf3978fc71458252e205c698a29ed411479fcd
HR Farmer <58487401+hrfarmer@users.noreply.github.com>
2024-07-23 16:48:42 -0500

    make popup close on esc press

--- app/frontend/src/components/PopupContainer.tsx
+++ app/frontend/src/components/PopupContainer.tsx
@@ -1,11 +1,23 @@
 import "assets/css/popup.css";
 import { PopupContext, PopupContextType } from "contexts/PopupContext";
 import { AnimatePresence, motion } from "framer-motion";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 export default function PopupContainer() {
   const { popup, setPopup } = useContext(PopupContext) as PopupContextType;
 
+  useEffect(() => {
+    function handleEsc(event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        setPopup(null);
+      }
+    }
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
   return (
     <AnimatePresence>
       {popup && (
