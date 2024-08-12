e59eaf621d5727940b8811c242e9149748090d32
hrfarmer <aychar@aychar.dev>
2024-08-11 23:21:59 -0500

    clean up footer

--- app/frontend/src/components/Footer.tsx
+++ app/frontend/src/components/Footer.tsx
@@ -1,30 +1,34 @@
 import discordLogo from "assets/images/discord-logo.png";
-import ytLogo from "assets/images/youtube-logo.png";
-import twitchLogo from "assets/images/twitch-logo.png";
-
 export default function Footer() {
-    return (
-        <footer>
-            <div className="footer">
-                {/* <div className="footer-block"> */}
-                    <a className="footer-link" href="https://discord.gg/tuE84PX9mx" target="_blank">
-                        <img className="footer-image prevent-select" src={discordLogo}></img>
-                    </a>
-                {/* </div> */}
-                {/* <div className="footer-block"> */}
-                    <a className="footer-link" href="https://twitch.tv/offlinechattournament" target="_blank">
-                        <img className="footer-image prevent-select" src={twitchLogo}></img>
-                    </a>
-                {/* </div> */}
-                {/* <div className="footer-block"> */}
-                    <a className="footer-link" href="https://www.youtube.com/@offlinechattournament" target="_blank">
-                        <img className="footer-image prevent-select" src={ytLogo}></img>
-                    </a>
-                {/* </div> */}
-                {/* <div className="footer-block"> */}
-                    <p className="footer-text prevent-select">developed by sheppsu and aychar</p>
-                {/* </div> */}
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="footer">
+        <a
+          className="footer-link"
+          href="https://discord.gg/wY9U8h6Hgj"
+          target="_blank"
+        >
+          <img className="footer-image prevent-select" src={discordLogo}></img>
+        </a>
+        <p className="footer-text prevent-select">
+          developed by{" "}
+          <a
+            href="https://github.com/Sheppsu"
+            target="_blank"
+            className="footer-text-link"
+          >
+            sheppsu
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/hrfarmer"
+            target="_blank"
+            className="footer-text-link"
+          >
+            aychar
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 }
