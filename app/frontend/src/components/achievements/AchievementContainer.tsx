4b160183f122e4c1686d39370cbc07477d1235f9
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-10-27 00:03:11 -0400

    add sorting

--- app/frontend/src/components/achievements/AchievementContainer.tsx
+++ app/frontend/src/components/achievements/AchievementContainer.tsx
@@ -7,6 +7,11 @@ import { getMyTeam } from "util/helperFunctions.ts";
 import { useContext } from "react";
 import { SessionContext } from "contexts/SessionContext.ts";
 import { WebsocketState } from "types/WebsocketStateType.ts";
+import { NavItems } from "components/achievements/AchievementNavigationBar.tsx";
+import {
+  AchievementCompletionType,
+  AnonymousAchievementCompletionType,
+} from "api/types/AchievementCompletionType.ts";
 
 function intersects(a: string[], b: string[]): boolean {
   for (const item of b) {
@@ -50,6 +55,73 @@ function matchesMode(
   return true;
 }
 
+function getGrouping(
+  sort: string,
+): [
+  string[],
+  (a: CompletedAchievementType) => string,
+  (a: CompletedAchievementType, b: CompletedAchievementType) => number,
+] {
+  const getMyCompletion = (
+    cs: (AchievementCompletionType | AnonymousAchievementCompletionType)[],
+  ) => {
+    for (const c of cs) {
+      if ("player" in c) {
+        return c;
+      }
+    }
+
+    return null;
+  };
+
+  const getTimestamp = (
+    cs: (AchievementCompletionType | AnonymousAchievementCompletionType)[],
+  ): number => {
+    const c = getMyCompletion(cs);
+    return c === null ? 0 : Date.parse(c.time_completed);
+  };
+
+  const getTimeGroup = (a: CompletedAchievementType): string => {
+    const timestamp = getTimestamp(a.completions);
+    const timeAgo = (Date.now() - timestamp) / 1000;
+    if (timeAgo < 60 * 60) return "Past hour";
+    else if (timeAgo < 60 * 60 * 24) return "Past 24 hours";
+    else if (timeAgo < 60 * 60 * 24 * 3) return "Past 3 days";
+    else if (timeAgo < 60 * 60 * 24 * 7) return "Past week";
+    else return "Over a week ago";
+  };
+
+  switch (sort) {
+    case "category":
+      return [["*"], (a) => a.category, (a, b) => a.name.localeCompare(b.name)];
+    case "completions":
+      return [
+        ["1+ completion(s)", "No completions"],
+        (a) => (a.completion_count > 0 ? "1+ completion(s)" : "No completions"),
+        (a, b) => a.completion_count - b.completion_count,
+      ];
+    case "player":
+      return [
+        ["*", "Not completed"],
+        (a) =>
+          a.completed
+            ? getMyCompletion(a.completions)!.player.user.username
+            : "Not completed",
+        (a, b) => a.name.localeCompare(b.name),
+      ];
+    case "date completed":
+      return [
+        ["*", "Not completed"],
+        (a) => (a.completed ? getTimeGroup(a) : "Not completed"),
+        (a, b) => getTimestamp(a.completions) - getTimestamp(b.completions),
+      ];
+    default:
+      throw new Error(
+        "unexpected sort type, was there a typo? missing a sort type?",
+      );
+  }
+}
+
 type CompletedAchievementType = AchievementExtendedType & {
   completed: boolean;
 };
@@ -76,6 +148,8 @@ export default function AchievementContainer({
       </div>
     );
 
+  const filters = state.achievementsFilter as NavItems;
+
   const achievements: CompletedAchievementType[] = baseAchievements.map(
     (a) => ({ ...a, completed: false }),
   );
@@ -93,14 +167,16 @@ export default function AchievementContainer({
     }
   }
 
-  const activeCategories = state.achievementsFilter.categories
+  const activeCategories = filters.categories
     .filter((item) => item.active)
     .map((item) => item.label);
-  const activeTags = state.achievementsFilter.tags
+  const activeTags = filters.tags
     .filter((item) => item.active)
     .map((item) => item.label);
   const searchFilter = state.achievementsSearchFilter.toLowerCase().split(" ");
 
+  const sort = filters.sort.filter((i) => i.active)[0].label;
+  const [groupSort, groupFunc, sortFunc] = getGrouping(sort);
   const sortedAchievements: { [key: string]: CompletedAchievementType[] } = {};
 
   for (const achievement of achievements) {
@@ -122,15 +198,36 @@ export default function AchievementContainer({
 
     if (state.hideCompletedAchievements && achievement.completed) continue;
 
-    if (!sortedAchievements[achievement.category])
-      sortedAchievements[achievement.category] = [];
-    sortedAchievements[achievement.category].push(achievement);
+    // get group to put this achievement into
+    const group = groupFunc(achievement);
+    if (!(group in sortedAchievements)) sortedAchievements[group] = [];
+
+    // insert into group sorted
+    let i = 0;
+    const items = sortedAchievements[group];
+    while (i < items.length && sortFunc(achievement, items[i]) > 0) {
+      i += 1;
+    }
+    sortedAchievements[group] = items
+      .slice(0, i)
+      .concat([achievement], items.slice(i, items.length));
   }
+
+  const getGroupIndex = (group: string) => {
+    const i = groupSort.indexOf(group);
+    return i == -1 ? groupSort.indexOf("*") : i;
+  };
+
+  const sortGroups = (g1: string, g2: string) => {
+    const i1 = getGroupIndex(g1);
+    const i2 = getGroupIndex(g2);
+    return i1 == i2 ? g1.localeCompare(g2) : i1 - i2;
+  };
 
   return (
     <div ref={scope} className="achievements-container">
       {Object.keys(sortedAchievements)
-        .sort((a, b) => a.localeCompare(b))
+        .sort(sortGroups)
         .map((key) => {
           return (
             <>
--- app/frontend/src/routes/achievements.tsx
+++ app/frontend/src/routes/achievements.tsx
@@ -5,10 +5,8 @@ import { SessionContext } from "contexts/SessionContext";
 import { Helmet } from "react-helmet";
 import { AchievementTeamExtendedType } from "api/types/AchievementTeamType";
 import AnimatedPage from "components/AnimatedPage";
-import { AchievementExtendedType } from "api/types/AchievementType";
-import { getMyTeam, toTitleCase } from "util/helperFunctions";
+import { getMyTeam } from "util/helperFunctions";
 import { AnimationScope, useAnimate } from "framer-motion";
-import Button from "components/Button";
 import { WebsocketState } from "types/WebsocketStateType.ts";
 import {
   StateDispatch,
@@ -17,6 +15,9 @@ import {
 } from "contexts/StateContext.ts";
 import "assets/css/achievements.css";
 import AchievementProgress from "components/achievements/AchievementProgress.tsx";
+import AchievementNavigationBar, {
+  getDefaultNav,
+} from "components/achievements/AchievementNavigationBar.tsx";
 
 function getTimeStr(delta: number) {
   const days = Math.floor((delta / (1000 * 60 * 60 * 24)) % 60);
@@ -87,182 +88,6 @@ function FullAchievementCompletionPage({
         />
       </div>
     </>
-  );
-}
-
-export type NavItem = {
-  label: string;
-  active: boolean;
-};
-
-export type NavItems = {
-  mode: NavItem[];
-  categories: NavItem[];
-  tags: NavItem[];
-};
-
-function getDefaultNav(achievements: AchievementExtendedType[]): NavItems {
-  const categories: string[] = [];
-  const tags: string[] = [];
-  for (const achievement of achievements) {
-    if (!categories.includes(achievement.category)) {
-      categories.push(achievement.category);
-    }
-
-    for (const tag of achievement.tags.split(",")) {
-      if (tag === "") continue;
-      if (tag.startsWith("mode-")) continue;
-
-      if (!tags.includes(tag)) {
-        tags.push(tag);
-      }
-    }
-  }
-
-  return {
-    mode: [
-      { label: "standard", active: true },
-      { label: "taiko", active: false },
-      { label: "mania", active: false },
-      { label: "catch", active: false },
-    ],
-    categories: categories.map((c) => ({ label: c, active: false })),
-    tags: tags.map((t) => ({ label: t, active: false })),
-  };
-}
-
-function AchievementNavigationBar({
-  state,
-  animate,
-  dispatchState,
-  achievements,
-}: {
-  state: WebsocketState | null;
-  animate: any; // i have no clue how to put the type for this
-  dispatchState: StateDispatch;
-  achievements: AchievementExtendedType[] | undefined;
-}) {
-  const [animating, setAnimating] = useState(false);
-  const [searchField, setSearchField] = useState<string>("");
-
-  useEffect(() => {
-    if (animating) {
-      (async () => {
-        await animate("div", { y: 10, opacity: 0 }, { duration: 0.2 });
-        await animate("div", { y: 0, opacity: 100 }, { duration: 0.2 });
-      })();
-
-      setAnimating(false);
-    }
-  }, [animating]);
-
-  function onReset() {
-    if (!achievements) return;
-
-    setAnimating(true);
-    setTimeout(() => {
-      dispatchState({ id: 5, achievementsFilter: getDefaultNav(achievements) });
-      dispatchState({ id: 6, achievementsSearchFilter: "" });
-      setSearchField("");
-    }, 225);
-  }
-
-  function onItemClick(category: keyof NavItems, label: string) {
-    if (state === null || state.achievementsFilter === null) return;
-
-    const newItems = { ...state.achievementsFilter };
-
-    if (category === "mode") {
-      for (const child of newItems.mode) {
-        child.active = child.label === label;
-      }
-
-      dispatchState({
-        id: 4,
-        mode: ["standard", "taiko", "catch", "mania"].indexOf(label),
-      });
-    } else {
-      for (const child of newItems[category]) {
-        if (child.label === label) {
-          child.active = !child.active;
-        }
-      }
-    }
-
-    setAnimating(true);
-    setTimeout(() => {
-      dispatchState({ id: 5, achievementsFilter: newItems });
-    }, 225);
-  }
-
-  return (
-    <div className="achievement-nav-bar">
-      {state === null ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <div className="achievement-nav-bar-input-row">
-            <input
-              type="text"
-              placeholder="Search"
-              name="input"
-              autoComplete="off"
-              value={searchField}
-              onChange={(e) => {
-                setSearchField(e.target.value);
-                dispatchState({
-                  id: 6,
-                  achievementsSearchFilter: e.target.value,
-                });
-              }}
-            />
-            <div className="achievement-nav-bar-checkbox">
-              <input
-                id="hide-completed"
-                type="checkbox"
-                style={{ width: 18 }}
-                onChange={(e) => {
-                  setAnimating(true);
-                  setTimeout(() => {
-                    dispatchState({
-                      id: 7,
-                      hideCompletedAchievements: e.target.checked,
-                    });
-                  }, 225);
-                }}
-              />
-              <p>Hide completed achievements</p>
-            </div>
-          </div>
-          {Object.entries(state.achievementsFilter ?? {}).map(
-            ([category, children]) => (
-              <div className="achievement-nav-bar-row" key={category}>
-                <p className="achievement-nav-bar-label">
-                  {toTitleCase(category)}
-                </p>
-                <div className="achievement-nav-bar-children">
-                  {children.map((item) => (
-                    <p
-                      key={item.label}
-                      className={
-                        "achievement-nav-bar-item" +
-                        (item.active ? " active" : "")
-                      }
-                      onClick={() =>
-                        onItemClick(category as keyof NavItems, item.label)
-                      }
-                    >
-                      {toTitleCase(item.label)}
-                    </p>
-                  ))}
-                </div>
-              </div>
-            ),
-          )}
-          <Button onClick={() => onReset()}>Reset to Default</Button>
-        </>
-      )}
-    </div>
   );
 }
 
