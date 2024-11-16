0dd5dbaff0db3cc8c2c43430ef43be2e1deea2d7
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-11-15 23:58:13 -0500

    add anonymized team names

--- app/frontend/src/components/achievements/LeaderboardCard.tsx
+++ app/frontend/src/components/achievements/LeaderboardCard.tsx
@@ -2,6 +2,7 @@ import "assets/css/team.css";
 
 import { useGetTeams } from "api/query";
 import { AchievementTeamType } from "api/types/AchievementTeamType";
+import { getAnonName } from "util/helperFunctions.ts";
 
 export default function LeaderboardCard() {
   const teamsResp = useGetTeams(true);
@@ -16,9 +17,12 @@ export default function LeaderboardCard() {
             : teamsResp.data === undefined
               ? "Error loading leaderboard"
               : teamsResp.data.map((team, i) => (
-                  <p>
-                    #{i + 1} - {team.points}pts
-                  </p>
+                  <>
+                    <p>
+                      #{i + 1}: {getAnonName(team.id)}
+                    </p>
+                    <p>{team.points}pts</p>
+                  </>
                 ))}
         </div>
       </div>
