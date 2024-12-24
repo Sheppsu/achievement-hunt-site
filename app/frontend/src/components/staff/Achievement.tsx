3e3a219bec125b8a7ee727e378c7990604c86f93
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2024-12-24 00:27:02 -0600

    add tags and votes display

--- app/frontend/src/components/staff/Achievement.tsx
+++ app/frontend/src/components/staff/Achievement.tsx
@@ -1,4 +1,5 @@
 import { StaffAchievementType } from "api/types/AchievementType.ts";
+import { BiSolidUpArrow, BiUpArrow } from "react-icons/bi";
 
 export default function Achievement({
   achievement,
@@ -10,6 +11,15 @@ export default function Achievement({
       <p className="staff__achievement__name">{achievement.name}</p>
       <p>{achievement.description}</p>
       <p className="staff__achievement__solution">{achievement.solution}</p>
+      <div className="staff__achievement__footer">
+        <div className="staff__achievement__footer__vote-container">
+          {achievement.vote_count}
+          <BiUpArrow />
+        </div>
+        {achievement.tags.split(",").map((tag) => (
+          <div className="staff__achievement__footer__tag">{tag}</div>
+        ))}
+      </div>
     </div>
   );
 }
