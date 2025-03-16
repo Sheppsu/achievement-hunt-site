d5203f3281a60d9aa0f4807d895486879eb4166f
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2025-03-16 15:49:57 -0400

    add user management command

---
+++ app/achievements/management/commands/manageuser.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from achievements.models import User, UserManager
+
+
+class Command(BaseCommand):
+    help = "Manage a user's attributes"
+
+    def add_arguments(self, parser):
+        parser.add_argument("user_id", type=int)
+        parser.add_argument("action", choices=["admin", "staff"])
+
+    def handle(self, *args, **options):
+        user = User.objects.filter(pk=options["user_id"]).first()
+
+        if user is None:
+            user = User.objects.create_user_from_id(options["user_id"])
+
+        if options["action"] == "admin":
+            user.is_admin = True
+        elif options["action"] == "staff":
+            user.is_achievement_creator = True
+
+        user.save()
