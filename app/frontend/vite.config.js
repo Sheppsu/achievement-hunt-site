01d3255a8433978ed1223e0ce4066bd16a735b0c
aychar <58487401+hrfarmer@users.noreply.github.com>
2025-04-14 21:16:43 -0500

    silence all module directive warnings

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
 import * as fs from "fs";
-
+import { defineConfig } from "vite";
 
 const config = JSON.parse(fs.readFileSync("tsconfig.json"));
 
 let aliases = {};
 for (const [k, v] of Object.entries(config.compilerOptions.paths)) {
-  aliases[k.substring(0, k.length-2)] = v[0].substring(1, v[0].length-2);
+  aliases[k.substring(0, k.length - 2)] = v[0].substring(1, v[0].length - 2);
 }
 
 export default defineConfig((cmd, mode) => {
@@ -16,16 +15,16 @@ export default defineConfig((cmd, mode) => {
       rollupOptions: {
         // ignore warning which doesn't matter
         onwarn(warning, warn) {
-          if (warning.code === "MODULE_LEVEL_DIRECTIVE" && warning.message.includes("@tanstack/react-query")) {
+          if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
             return;
           }
           warn(warning);
-        }
-      }
+        },
+      },
     },
     base: "/static",
     resolve: {
-      alias: aliases
-    }
+      alias: aliases,
+    },
   };
 });
