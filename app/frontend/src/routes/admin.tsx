82736f1896b89995f9564e8ac92dd4afa12c316d
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2025-05-29 16:33:35 -0400

    ensure admin on admin page

--- app/frontend/src/routes/admin.tsx
+++ app/frontend/src/routes/admin.tsx
@@ -3,8 +3,11 @@ import "assets/css/admin.css";
 import React from "react";
 import BatchesCard from "components/admin/BatchesCard.tsx";
 import AnnouncementCreationCard from "components/admin/AnnouncementCreationCard.tsx";
+import { useAuthEnsurer } from "util/auth.ts";
 
 export default function AdminPage() {
+  useAuthEnsurer().ensureAdmin();
+
   return (
     <>
       <Helmet>
