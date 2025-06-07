e25a488250c868aba9f95e1ed0f7be13e6238818
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2025-06-07 18:11:28 -0400

    add staff to credits page

--- app/frontend/src/routes/credit.tsx
+++ app/frontend/src/routes/credit.tsx
@@ -11,7 +11,163 @@ export default function Credit() {
 
       <div className="cards-container">
         <div className="cards-container__column">
-          <div className="card">
+          <div className="card no-scroll">
+            <h1>Capture The Achievement 2</h1>
+            <h1 className="staff-header">Host</h1>
+            <div className="staff-container">
+              <a
+                href="https://sheppsu.me"
+                target="_blank"
+                className="external-link staff"
+              >
+                Sheppsu
+              </a>
+            </div>
+            <h1 className="staff-header">Banner graphic</h1>
+            <div className="staff-container">
+              <a
+                href="https://osu.ppy.sh/users/18607342"
+                target="_blank"
+                className="external-link staff"
+              >
+                Kheops
+              </a>
+            </div>
+            <h1 className="staff-header">Website developers</h1>
+            <div className="staff-container">
+              <a
+                href="https://sheppsu.me"
+                target="_blank"
+                className="external-link staff"
+              >
+                Sheppsu
+              </a>
+              <a
+                href="https://aychar.dev"
+                target="_blank"
+                className="external-link staff"
+              >
+                aychar
+              </a>
+              <a
+                href="https://github.com/Rinne0333"
+                target="_blank"
+                className="external-link staff"
+              >
+                Rinne
+              </a>
+            </div>
+            <h1 className="staff-header">Achievement masterminds</h1>
+            <div className="staff-container">
+              <a
+                href="https://sheppsu.me"
+                target="_blank"
+                className="external-link staff"
+              >
+                Sheppsu
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/11106342"
+                target="_blank"
+                className="external-link staff"
+              >
+                AlexRLJones
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/10238680"
+                target="_blank"
+                className="external-link staff"
+              >
+                chromb
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/11758404"
+                target="_blank"
+                className="external-link staff"
+              >
+                Belladonna
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/6147964"
+                target="_blank"
+                className="external-link staff"
+              >
+                Dragon20942
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/18319269"
+                target="_blank"
+                className="external-link staff"
+              >
+                FerbyEats
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/7183087"
+                target="_blank"
+                className="external-link staff"
+              >
+                Daanny
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/17465623"
+                target="_blank"
+                className="external-link staff"
+              >
+                yandax
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/7772622"
+                target="_blank"
+                className="external-link staff"
+              >
+                rc4322 / TamamoLover
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/14177677"
+                target="_blank"
+                className="external-link staff"
+              >
+                Whatsoever545
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/14208558"
+                target="_blank"
+                className="external-link staff"
+              >
+                Alanko
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/11153810"
+                target="_blank"
+                className="external-link staff"
+              >
+                Anonymoose
+              </a>
+              <a
+                href="https://aychar.dev"
+                target="_blank"
+                className="external-link staff"
+              >
+                aychar
+              </a>
+              <a
+                href="https://github.com/Rinne0333"
+                target="_blank"
+                className="external-link staff"
+              >
+                Rinne
+              </a>
+              <a
+                href="https://osu.ppy.sh/users/1699875"
+                target="_blank"
+                className="external-link staff"
+              >
+                Remyria
+              </a>
+            </div>
+          </div>
+          <div className="card no-scroll">
+            <h1>Capture The Achievement</h1>
             <h1 className="staff-header">Host</h1>
             <div className="staff-container">
               <a
